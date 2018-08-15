67d886bc18f6cb70fdf10509db00171fd0fa7e60
varunjammula <varunjammula@gmail.com>
2018-08-14 19:48:08 -0700

    Typing error

--- src/op_ros.py
+++ src/op_ros.py
@@ -44,7 +44,7 @@ openpose = OpenPose(params)
 class ROS2OpenCV2(object):
 
     def __init__(self, node_name):
-        self.node_name = node_nam
+        self.node_name = node_name
 
         rospy.init_node(node_name)
         rospy.loginfo("Starting node at " + str(node_name))
