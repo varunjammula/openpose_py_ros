1c66015d9c405494b9cd8da22f4c594ca3092976
Varun Jammula <varunjammula@gmail.com>
2019-01-02 13:07:10 +0530

    ROI publisher enabled.

--- src/op_ros.py
+++ src/op_ros.py
@@ -6,7 +6,7 @@ from sys import platform
 import cv2
 import rospy
 from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, RegionOfInterest
 import numpy as np
 
 # dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -31,6 +31,8 @@ params["alpha_pose"] = 0.6
 params["scale_gap"] = 0.3
 params["scale_number"] = 1
 params["render_threshold"] = 0.05
+params["tracking"] = 5
+params["number_people_max"] = 1
 # If GPU version is built, and multiple GPUs are available, set the ID here
 params["num_gpu_start"] = 0
 params["disable_blending"] = False
@@ -58,47 +60,57 @@ class ROS2OpenCV2(object):
         self.bridge = CvBridge()
         self.frame = None
         self.start = 0
+        self.roi_pub = rospy.Publisher('/roi', RegionOfInterest, queue_size=10)
         self.image_sub = rospy.Subscriber(img_topic_name, Image, self.image_callback)
 
         while not rospy.is_shutdown():
             if self.frame is not None:
+                # rospy.loginfo("Processing image")
                 self.start = time.time()
-                keypoints, output_image = openpose.forward(self.frame, True)
+                keypoints = openpose.forward(self.frame, False)
                 if len(keypoints) > 0:
                     b_parts = keypoints[0]
                     pts = []
                     for i in range(len(b_parts)):
-                        print 'x: {}, y:{}, confidence: {}'.format(b_parts[i][0], b_parts[i][1], b_parts[i][2])
+                        #print 'x: {}, y:{}, confidence: {}'.format(b_parts[i][0], b_parts[i][1], b_parts[i][2])
                         if b_parts[i][2] > 0:
                             pts.append((b_parts[i][0], b_parts[i][1]))
 
                     points_matrix = np.array(pts).reshape((-1, 1, 2)).astype(np.int32)
                     self.rect = cv2.boundingRect(points_matrix)
+                    ROI = RegionOfInterest()
                     if self.rect is not None:
-                        cv2.rectangle(output_image, (self.rect[0], self.rect[1]),
+                        ROI.x_offset = int(self.rect[0])
+                        ROI.y_offset = int(self.rect[1])
+                        ROI.width = int(self.rect[2])
+                        ROI.height = int(self.rect[3])
+                        ROI.do_rectify = True
+                        cv2.rectangle(self.frame, (self.rect[0], self.rect[1]),
                                       (self.rect[0] + self.rect[2], self.rect[1] + self.rect[3]), (0, 255, 0), 1)
-                #output_image = self.frame
+                    self.roi_pub.publish(ROI)
+
                 # Compute the time for this loop and estimate CPS as a running average
                 end = time.time()
                 duration = end - self.start
                 fps = int(1.0 / duration)
 
-                cv2.putText(output_image, "CPS: " + str(fps), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                cv2.putText(self.frame, "FPS: " + str(fps), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                             (255, 255, 0))
 
                 # Update the image display
-                cv2.imshow(self.node_name, output_image)
+                cv2.imshow(self.node_name, self.frame)
 
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     return
+            #self.frame = None
 
     def image_callback(self, data):
         # Convert the ROS image to OpenCV format using a cv_bridge helper function
+        # rospy.loginfo("Image callback!!")
         self.frame = self.convert_image(data)
 
 
     def convert_image(self, ros_image):
-        # Use cv_bridge() to convert the ROS image to OpenCV format
         try:
             frame = self.bridge.imgmsg_to_cv2(ros_image, "bgr8")
             return frame
